18da062585bb1703902ba57fa07e347f2f145950
Aradhana Kumari <145246174+Aradhana2602@users.noreply.github.com>
2024-07-12 09:17:33 +0530

    Update first.cpp

--- first.cpp
+++ first.cpp
@@ -1,83 +1,133 @@
-// Implement a class Complex which represents the Complex Number data type. Implement the
-// following
-// 1. Constructor (including a default constructor which creates the complex number 0+0i).
-// 2. Overload operator+ to add two complex numbers.
-// 3. Overload operator* to multiply two complex numbers.
-// 4. Overload operators << and >> to print and read Complex Numbers.
+/*
+Implement a class Complex which represents the Complex Number data type. Implement the
+following
+1. Constructor (including a default constructor which creates the complex number 0+0i).
+2. Overload operator+ to add two complex numbers.
+3. Overload operator* to multiply two complex numbers.
+4. Overload operators << and >> to print and read Complex Numbers.
+*/
 #include <iostream>
-
 using namespace std;
 
+// Class definition for Complex numbers
 class Complex {
-private:
-    double real;
-    double imaginary;
-// double real: Stores the real part of the complex number.
-// double imaginary: Stores the imaginary part of the complex number.
+    double real; // Real part of the complex number
+    double img;  // Imaginary part of the complex number
 
 public:
     // Constructors
-    Complex() : real(0.0), imaginary(0.0) {}
-    // Default constructor initializes real and imaginary to '0.0'
-    Complex(double r, double i) : real(r), imaginary(i) {}
-    // Parameterized constructor initializes real to r and imaginary to i.
+    Complex(); // Default constructor
+    Complex(double r, double i); // Parameterized constructor
 
-    // Operator overloading
-    Complex operator + (const Complex& other)  const {
-        return Complex(real + other.real, imaginary + other.imaginary);
-    }
-    // Return Type: Complex: Indicates that the function will return an object of type Complex.
-    // Operator: operator+: This defines the addition operator (+)
-    // Parameter: const Complex& other: This parameter is a reference to another Complex object (other).
-    // It's marked as const to ensure that the other object is not modified within the function, thus ensuring const-correctness and preventing accidental modifications.
-    // const Member Function: The const keyword at the end of the function declaration (const) ensures that 
-    // this member function does not modify the state of the object (*this), which is important for maintaining object integrity and enabling const-correctness.
-    Complex operator * (const Complex& other) const {
-        // operator*:multiply two complex numbers using formula:
-        // (a+bi)×(c+di)=(ac−bd)+(ad+bc)i
-        double r = real * other.real - imaginary * other.imaginary;
-        double i = real * other.imaginary + imaginary * other.real;
-        return Complex(r, i);
-    }
+    // Operator overloading for addition and multiplication
+    Complex operator + (const Complex&); // Addition operator overload
+    Complex operator * (const Complex&); // Multiplication operator overload
 
-    // Stream insertion and extraction operators
-    friend ostream& operator << (ostream& out, const Complex& c) {
-        // & used to creat a referance of a variable
-        // const specifies that the object is qualfied to not modifiable. ensures that the value of the object can  not be changed though this referance.
-        out << c.real << " + " << c.imaginary << "i";
-        return out;
-    }
-
-    friend istream& operator >> (istream& in, Complex& c) {
-        // const Complex& is used to ensure that a function can acces the object efficiently without allowing unintended modification  
-        // friend function is a function that is not a member of a class but has access to the private and protected members of the class if it is declared as a friend within that class.
-
-        cout << "Enter real part: ";
-        in >> c.real;
-        cout << "Enter imaginary part: ";
-        in >> c.imaginary;
-        return in;
-    }
+    // Friend functions for input and output
+    friend istream& operator >> (istream&, Complex&);
+    friend ostream& operator << (ostream&, const Complex&);
 };
 
+// Default constructor initializes complex number to 0 + 0i
+Complex::Complex() {
+    real = 0;
+    img = 0;
+}
+
+// Parameterized constructor initializes complex number to specified real and imaginary parts
+Complex::Complex(double r, double i) {
+    real = r;
+    img = i;
+}
+
+// Overloading >> operator for reading Complex numbers from input
+istream& operator >> (istream& in, Complex& c) {
+    cout << "Enter real part: ";
+    in >> c.real;
+    cout << "Enter imaginary part: ";
+    in >> c.img;
+    return in;
+}
+
+// Overloading << operator for printing Complex numbers to output
+ostream& operator << (ostream& out, const Complex& c) {
+    out << c.real << " + i" << c.img << "\n";
+    return out;
+}
+
+// Overloading + operator for adding two Complex numbers
+Complex Complex::operator + (const Complex& c) {
+    Complex temp;
+    temp.real = real + c.real; // Adding real parts
+    temp.img = img + c.img;    // Adding imaginary parts
+    return temp;               // Returning the result as a new Complex object
+}
+
+// Overloading * operator for multiplying two Complex numbers
+Complex Complex::operator * (const Complex& c) {
+    Complex tmp;
+    tmp.real = real * c.real - img * c.img; // Multiplying using complex number multiplication formula
+    tmp.img = real * c.img + img * c.real;
+    return tmp; // Returning the result as a new Complex object
+}
+
+// Main function
 int main() {
-    Complex c1(3.0, 4.0);
-    Complex c2(2.0, -1.0);
+    Complex C1, C2, C3, C4; // Declaring four Complex objects to store complex numbers
+    int flag = 1; // Flag to control program execution
+    char b;
 
-    // Addition
-    Complex sum = c1 + c2;
-    cout << "Sum: " << sum << endl;
+    // Outer loop to continue running the program until flag is set to 0
+    while (flag == 1) {
+        // Prompting user to enter real and imaginary parts for Complex Number 1
+        cout << "Enter Real and Imaginary part of the Complex Number 1:\n";
+        cin >> C1;
 
-    // Multiplication
-    Complex product = c1 * c2;
-    // Demonstrates the use of operator+ to add two complex numbers (c1 and c2).
-    cout << "Product: " << product << endl;
+        // Prompting user to enter real and imaginary parts for Complex Number 2
+        cout << "Enter Real and Imaginary part of the Complex Number 2:\n";
+        cin >> C2;
 
-    // Input and output using stream operators
-    Complex c3;
-    cout << "Enter a complex number:" << endl;
-    cin >> c3;
-    cout << "You entered: " << c3 << endl;
+        int operation; // Variable to store user's operation choice
+
+        // Displaying Complex numbers entered by user
+        cout << "Complex Number 1: " << C1 << endl;
+        cout << "Complex Number 2: " << C2 << endl;
+
+        // Displaying menu options
+        cout << "********** MENU **********" << endl;
+        cout << "1. Addition of Complex Numbers" << endl;
+        cout << "2. Multiplication of Complex Numbers" << endl;
+        cout << "3. Exit" << endl;
+        cout << "Enter your choice from the MENU (1 to 3): ";
+        cin >> operation;
+
+        // Performing operation based on user's choice
+        if (operation == 1) {
+            // Addition operation
+            C3 = C1 + C2;
+            cout << "Addition Result: " << C3 << endl;
+        } else if (operation == 2) {
+            // Multiplication operation
+            C4 = C1 * C2;
+            cout << "Multiplication Result: " << C4 << endl;
+        } else if (operation == 3) {
+            // Exiting the program
+            cout << "Exiting program. Thank you!\n";
+            flag = 0; // Setting flag to 0 to exit outer loop
+        } else {
+            // Invalid choice
+            cout << "Invalid choice! Please enter again.\n";
+        }
+
+        // Asking user if they want to perform another operation
+        if (flag != 0) {
+            cout << "Do you want to perform another operation? (y/n): ";
+            cin >> b;
+            if (b != 'y' && b != 'Y') {
+                flag = 0; // Setting flag to 0 to exit outer loop
+            }
+        }
+    }
 
     return 0;
 }
